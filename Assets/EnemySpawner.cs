3eb86e930eb80e791dc9aedb0bf2b0c4b9a476bd
Zayaan <72232563+grizzledwarvet23@users.noreply.github.com>
2023-09-24 13:30:48 -0500

    tree walls

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -31,7 +31,24 @@ public class EnemySpawner : MonoBehaviour
 
     IEnumerator SpawnEnemies() {
         yield return new WaitForSeconds(spawnDelay);
-        //spawn enemies in the corresponding bounds:
+        //spawn enemy in the bounds; heres how it works:
+        //transformParents children are labeled 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+        //those children have children TL, TR, BL, BR representing the corners of the bounds
+        //spawn the enemy  randomly at the bounds:
+        if(GameManager.instance.currentWave == 1) {
+            //random number from 1 to 4
+            int enemyNum = Random.Range(1, 5);
+            GameObject enemy;
+            if(enemyNum == 1) {
+                enemy = Instantiate(enemyOne, transformsParent.transform.GetChild(0).GetChild(0).position, Quaternion.identity);
+            } else if(enemyNum == 2) {
+                enemy = Instantiate(enemyTwo, transformsParent.transform.GetChild(0).GetChild(1).position, Quaternion.identity);
+            } else if(enemyNum == 3) {
+                enemy = Instantiate(enemyThree, transformsParent.transform.GetChild(0).GetChild(2).position, Quaternion.identity);
+            } else {
+                enemy = Instantiate(enemyFour, transformsParent.transform.GetChild(0).GetChild(3).position, Quaternion.identity);
+            }
+        }
 
     }
 
